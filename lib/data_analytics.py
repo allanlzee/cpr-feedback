944d4a3a3f487f813c1b01aed1352474519b9b1a
Allan Zhou <allan.zhou@mail.utoronto.ca>
2024-06-19 14:04:04 -0400

    data analytics graphs produced

--- lib/data_analytics.py
+++ lib/data_analytics.py
@@ -3,7 +3,9 @@ import pandas as pd
 import os
 from datetime import datetime
 
-df = pd.read_csv("C:\\Users\\allan\\Documents\\cpr-feedback\\lib\\testing.csv")
+name = input("Name of Participant: ").lower()
+
+df = pd.read_csv("C:\\Users\\allan\\Documents\\cpr-feedback\\lib\\testing-{}.csv".format(name))
 
 plt.figure(figsize=(10, 6))
 
@@ -27,7 +29,7 @@ for filename in os.listdir(output_directory):
         if 'm{}-d{}-v'.format(datetime.now().month, datetime.now().day) in filename:
             graph_number += 1 
 
-output_path = os.path.join(output_directory, 'm{}-d{}-v{}.png'.format(datetime.now().month, datetime.now().day, graph_number))
+output_path = os.path.join(output_directory, 'm{}-d{}-v{}-{}.png'.format(datetime.now().month, datetime.now().day, graph_number, name))
 plt.savefig(output_path)
 
 plt.show()
